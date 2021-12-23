a183a910921bf5c81a845e8473eb9de6d88e8f33
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-12-23 09:49:28 +0530

    feat: add TheHeader component

---
+++ components/templates/TheHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="tw-flex tw-justify-around tw-items-center tw-bg-gradient-to-br tw-from-white tw-to-emerald-50 tw-h-12">
+    <div>
+      <nuxt-link to="/">Indian Law</nuxt-link>
+    </div>
+    <div class="tw-flex tw-justify-start tw-items-center tw-gap-4">
+      <nuxt-link to="/central">Central Law</nuxt-link>
+      <nuxt-link to="/state">State Law</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
