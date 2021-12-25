5a3ba71bde9429f240a605d93c2d82eeed162236
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-12-25 21:18:57 +0530

    feat: add social media icons in Header

--- components/templates/TheHeader.vue
+++ components/templates/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="header-container">
+  <header class="header-container tw-flex tw-justify-around tw-items-center tw-bg-gradient-to-br tw-from-white tw-h-12">
     <div>
       <nuxt-link to="/">Indian Law</nuxt-link>
     </div>
@@ -7,7 +7,46 @@
       <nuxt-link to="/central">Central Law</nuxt-link>
       <nuxt-link to="/state">State Law</nuxt-link>
     </div>
-  </div>
+    <div class="tw-text-slate-400">
+      <a
+        href="https://github.com/ManasMadrecha/indianlaw"
+        target="_blank"
+      >
+        <span class="tw-sr-only">Indian Law GitHub</span>
+        <atomsAppIcon
+          icon="mdiGithub"
+          class="tw-w-6 tw-h-6"
+        ></atomsAppIcon>
+      </a>
+      <a
+        href="https://instagram.com/ManasMadrecha"
+        target="_blank"
+      >
+        <span class="tw-sr-only">Manas Madrecha Instagram</span>
+        <atomsAppIcon
+          icon="mdiInstagram"
+          class="tw-w-6 tw-h-6"
+        ></atomsAppIcon>
+      </a>
+      <a
+        href="https://www.linkedin.com/in/manasmadrecha/"
+        target="_blank"
+      >
+        <span class="tw-sr-only">Manas Madrecha Linkedin</span>
+        <atomsAppIcon
+          icon="mdiLinkedin"
+          class="tw-w-6 tw-h-6"
+        ></atomsAppIcon>
+      </a>
+      <a href="mailto:madrechamanas@gmail.com">
+        <span class="tw-sr-only">Manas Madrecha Email</span>
+        <atomsAppIcon
+          icon="mdiEmail"
+          class="tw-w-6 tw-h-6"
+        ></atomsAppIcon>
+      </a>
+    </div>
+  </header>
 </template>
 
 <script>
@@ -15,14 +54,10 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.header-container {
-  @apply tw-flex tw-justify-around tw-items-center tw-bg-gradient-to-br tw-from-white tw-h-12;
-}
-
 @each $theme-color in $theme-colors {
   .theme-#{$theme-color} {
     .header-container {
-      @apply tw-to-#{$theme-color}-50; 
+      @apply tw-to-#{$theme-color}-50;
     }
   }
 }
