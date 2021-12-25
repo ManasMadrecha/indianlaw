6ad41937f767ace12503afae82a976ec72033a47
CA Manas Madrecha <65993481+ManasMadrecha@users.noreply.github.com>
2021-12-25 21:18:30 +0530

    feat: add AppIcon component
    - can be used even in markdown (as just name of icon is needed as prop)

---
+++ components/atoms/AppIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <svg
+    viewBox="0 0 24 24"
+    v-if="iconPath"
+    class="tw-inline tw-fill-current"
+  >
+    <path :d="iconPath"></path>
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    iconPath() {
+      return require("@mdi/js")[this.icon];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
